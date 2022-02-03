8daddc40131219aa6ac8790bb74480c6aaeb4f56
Joshua Marsh <joshua@themarshians.com>
2022-02-03 09:53:36 -0700

    allow for different buttons and to run forever

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ import threading
 import pyautogui
 import pynput
 
+
 class Timer:
-    def __init__(self, scale):
+    def __init__(self, scale, button, forever):
         self.running = False
         self.clicks = 0
         self.scale = scale
+        self.button = button
+        self.forever = forever
         self.start = time.time()
 
     def run(self):
@@ -27,8 +30,14 @@ class Timer:
             time.sleep(1)
             countdown -= 1
 
-        while self.running and time.time() - self.start < self.scale * 60:
-            pyautogui.click()
+        while self.running and (self.forever or time.time() - self.start < self.scale * 60):
+            if self.button == "LEFT_MOUSE":
+                pyautogui.click()
+            elif self.button == "RIGHT_MOUSE":
+                pyautogui.click(button='right')
+            else:
+                pyautogui.press(self.button)
+
             self.clicks += 1
             when = 0.6 + random.uniform(0.0, 0.2)
             yield {
@@ -39,11 +48,10 @@ class Timer:
             }
             time.sleep(when)
 
+
 class AutoClicker:
     def __init__(self):
         self.root = tkinter.Tk()
-        self.msgTemplate = "Hello! This will simulate clicking the left mouse button. Once you click start, it will count down for 5 seconds and then click the left mouse button for {} minutes."
-        self.msg = tkinter.StringVar()
         self.scale = tkinter.IntVar()
         self.time = tkinter.StringVar()
 
@@ -52,44 +60,50 @@ class AutoClicker:
             self.pause)
 
         self.l = pynput.keyboard.Listener(
-            on_release = self.for_canonical(hotkey.release),
-            on_press   = self.for_canonical(hotkey.press),
+            on_release=self.for_canonical(hotkey.release),
+            on_press=self.for_canonical(hotkey.press),
         )
         self.l.start()
 
     def for_canonical(self, f):
         return lambda k: f(self.l.canonical(k))
 
-    def scale_changed(self, to):
-        self.msg.set(self.msgTemplate.format(self.scale.get()))
-
     def ui(self):
         self.root.title("Auto Clicker")
-        self.root.geometry("500x500")
         self.scale.set(5)
-        self.scale_changed(5)
-        m = tkinter.Message(self.root, width=500,
-                            textvariable=self.msg)
-        m.pack()
-
-
-        s = tkinter.Scale(self.root, from_=1, to=60, variable=self.scale,
-                          orient=tkinter.HORIZONTAL, length=500,
-                          command=self.scale_changed)
-        s.pack()
-    
 
         self.b = tkinter.Button(self.root, text="Start", command=self.start)
-        self.b.pack()
+        self.b.grid(row=0, column=0)
 
         self.p = tkinter.Button(self.root, text="Pause", command=self.pause)
-        self.p.pack()
+        self.p.grid(row=0, column=1)
 
         self.time.set("Click start to begin!")
-        l = tkinter.Label(self.root, width=500, textvariable=self.time)
-        l.pack()
+        l = tkinter.Label(self.root, textvariable=self.time)
+        l.grid(row=1, column=0, columnspan=2)
+
+        self.key = tkinter.StringVar()
+        self.key.set("LEFT_MOUSE")
+        k = tkinter.Entry(self.root, textvariable=self.key)
+        k.grid(row=3, column=0, columnspan=2)
+
+        self.forever = tkinter.IntVar()
+        check = tkinter.Checkbutton(self.root, text='Forever', variable=self.forever,
+                                    onvalue=1, offvalue=0, command=self.check_forever)
+        check.grid(row=4, column=0, columnspan=2)
+
+        self.s = tkinter.Scale(self.root, from_=1, to=60, variable=self.scale,
+                               orient=tkinter.HORIZONTAL)
+        self.s.grid(row=5, column=0, columnspan=2)
 
         self.root.mainloop()
+
+    def check_forever(self):
+        print(f"Checking forever {self.forever}")
+        if self.forever.get() == 1:
+            self.s.grid_forget()
+        else:
+            self.s.grid(row=6, column=0, columnspan=2)
 
     def pause(self):
         self.go = False
@@ -99,7 +113,7 @@ class AutoClicker:
         self.go = True
         self.p.configure(text="Pause", command=self.pause)
         threading.Thread(target=self.clicker).start()
-        
+
     def stop(self):
         self.go = False
         self.time.set("Click start to begin!")
@@ -108,13 +122,14 @@ class AutoClicker:
     def start(self):
         self.b.configure(text="Stop", command=self.stop)
         self.go = True
-        self.t = Timer(self.scale.get())
+        self.t = Timer(self.scale.get(), self.key.get(),
+                       self.forever.get() == 1)
         threading.Thread(target=self.clicker).start()
 
     def keys(self, key):
         if key == pynput.keyboard.Key.esc and self.go:
             self.pause()
-        
+
     def clicker(self):
         for msg in self.t.run():
             if msg["countdown"] > 0:
@@ -126,12 +141,13 @@ class AutoClicker:
             if not self.go:
                 return
         self.stop()
-                              
+
 
 def main():
     ac = AutoClicker()
     ac.ui()
-    
+
+
 if __name__ == "__main__":
     random.seed()
     main()
